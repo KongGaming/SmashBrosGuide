56a5b89078bdd11a25150581b0befecd472f4333
BULENDO <Oscar.Bulenda@sfx.act.edu>
2018-06-05 10:24:05 +1000

    first

--- app/src/main/java/com/example/bulendo/smashbrosguide/Characters.java
+++ app/src/main/java/com/example/bulendo/smashbrosguide/Characters.java
@@ -2,18 +2,12 @@ package com.example.bulendo.smashbrosguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainMenu extends AppCompatActivity {
+public class Characters extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-    }
-
-    public void characters(View view) {
-
-
+        setContentView(R.layout.activity_characters);
     }
 }
